f496fe1ba843fc29036c1d5a8fd5e18285dd8269
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2023-03-10 19:01:31 +0100

    added product object

--- src/main/java/com/mintyn/inventorymanagement/models/orderItem/OrderItem.java
+++ src/main/java/com/mintyn/inventorymanagement/models/orderItem/OrderItem.java
@@ -1,7 +1,10 @@
 package com.mintyn.inventorymanagement.models.orderItem;
 
+import com.mintyn.inventorymanagement.models.product.Product;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +22,8 @@ public class OrderItem {
     @Id
     private int id;
 
-    private String product;
+    @ManyToOne
+    private Product product;
 
     private int quantity;
 
