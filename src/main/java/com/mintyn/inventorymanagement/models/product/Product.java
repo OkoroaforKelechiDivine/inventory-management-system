926be31a17da8beb518666dd23b9179f9bd44d25
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2023-03-10 18:59:42 +0100

    worked on product file

---
+++ src/main/java/com/mintyn/inventorymanagement/models/product/Product.java
@@ -0,0 +1,29 @@
+package com.mintyn.inventorymanagement.models.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private int quantity;
+}
