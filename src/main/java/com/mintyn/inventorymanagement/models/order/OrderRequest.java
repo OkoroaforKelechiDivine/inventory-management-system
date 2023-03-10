0ee1fae1f1893b95bb247fc181d39204f51bf167
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2023-03-10 21:54:48 +0100

    added model for products and order

---
+++ src/main/java/com/mintyn/inventorymanagement/models/order/OrderRequest.java
@@ -0,0 +1,27 @@
+package com.mintyn.inventorymanagement.models.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class OrderRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int customerId;
+
+    private String customerName;
+
+    private String customerPhoneNumber;
+
+    private int productId;
+}
