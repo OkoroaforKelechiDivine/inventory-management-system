73a61effe400a07f49513314e6bbd6ec594bdecf
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2023-03-11 06:34:35 +0100

    created class exceptions

---
+++ src/main/java/com/mintyn/inventorymanagement/exception/OutOfStockException.java
@@ -0,0 +1,7 @@
+package com.mintyn.inventorymanagement.exception;
+
+public class OutOfStockException extends Exception{
+    public OutOfStockException(String message) {
+        super(message);
+    }
+}
