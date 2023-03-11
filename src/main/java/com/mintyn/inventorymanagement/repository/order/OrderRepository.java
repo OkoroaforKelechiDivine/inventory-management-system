1d0b4e035f711d6967bc3f358b160ac76878895a
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2023-03-11 18:37:32 +0100

    created exception

--- src/main/java/com/mintyn/inventorymanagement/repository/order/OrderRepository.java
+++ src/main/java/com/mintyn/inventorymanagement/repository/order/OrderRepository.java
@@ -4,13 +4,10 @@ import com.mintyn.inventorymanagement.models.order.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderItem, Integer> {
-
-//    List<OrderItem> findByOrderDateBetween(LocalDateTime atStartOfDay, LocalDateTime atStartOfDay1);
 
     List<OrderItem> findAllByProductIdAndTotalPriceIsNull(int id);
 }
