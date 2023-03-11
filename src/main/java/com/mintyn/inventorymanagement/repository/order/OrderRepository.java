dbd0d0cd93be564be88e0a8735d0cfcd597c3fe0
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2023-03-11 08:24:55 +0100

    fixes

--- src/main/java/com/mintyn/inventorymanagement/repository/order/OrderRepository.java
+++ src/main/java/com/mintyn/inventorymanagement/repository/order/OrderRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<OrderItem, Integer> {
 
-    List<OrderItem> findByOrderDateBetween(LocalDateTime atStartOfDay, LocalDateTime atStartOfDay1);
+//    List<OrderItem> findByOrderDateBetween(LocalDateTime atStartOfDay, LocalDateTime atStartOfDay1);
 
     List<OrderItem> findAllByProductIdAndTotalPriceIsNull(int id);
 }
