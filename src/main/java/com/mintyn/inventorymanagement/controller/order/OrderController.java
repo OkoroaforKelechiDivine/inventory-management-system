e4f5c9883c67d054d4e9340f0a5e970344f046f1
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2023-03-11 06:29:36 +0100

    worked on order controller

--- src/main/java/com/mintyn/inventorymanagement/controller/order/OrderController.java
+++ src/main/java/com/mintyn/inventorymanagement/controller/order/OrderController.java
@@ -1,24 +1,53 @@
 package com.mintyn.inventorymanagement.controller.order;
 
 
-import com.mintyn.inventorymanagement.models.order.Order;
+import com.mintyn.inventorymanagement.dto.OrderReport;
+import com.mintyn.inventorymanagement.exception.OutOfStockException;
+import com.mintyn.inventorymanagement.models.order.OrderItem;
 import com.mintyn.inventorymanagement.models.order.OrderRequest;
+import com.mintyn.inventorymanagement.repository.order.OrderReportRepository;
+import com.mintyn.inventorymanagement.repository.order.OrderRepository;
 import com.mintyn.inventorymanagement.service.order.OrderServiceImplementation;
+import com.mintyn.inventorymanagement.service.order.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/orderItems")
 public class OrderController {
 
     @Autowired
     private OrderServiceImplementation orderServiceImplementation;
 
-    @PostMapping("/orders")
-    public Order placeOrder(@RequestBody OrderRequest orderRequest) {
-        return orderServiceImplementation.placeOrder(orderRequest);
+    @Autowired
+    private OrderReportRepository orderReportRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ReportService reportService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> createOrder(@RequestBody OrderRequest orderRequest) throws Exception {
+        try{
+            OrderItem newOrderItem = orderServiceImplementation.createOrder(orderRequest);
+            return ResponseEntity.ok(newOrderItem);
+        }
+        catch (OutOfStockException outOfStockException){
+            return ResponseEntity.badRequest().body(outOfStockException.getMessage());
+        }
+    }
+
+    @GetMapping("/reports")
+    public List<OrderReport> getOrderReport(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+        return reportService.generateOrderReport(startDate, endDate);
     }
 }
